4a3168e8aba89f8d944921e722014464a1268d60
mchinddd <chinh05102005@gmail.com>
2025-07-31 09:48:19 +0700

    Token-based

--- app/src/main/java/com/example/asm_ad/LoginActivity.java
+++ app/src/main/java/com/example/asm_ad/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.asm_ad;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -25,6 +26,24 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         btnSignin = findViewById(R.id.btnSignin);
 
+        SharedPreferences prefs = getSharedPreferences("UserSession", MODE_PRIVATE);
+        boolean isLoggedIn = prefs.getBoolean("isLoggedIn", false);
+        long lastActiveTime = prefs.getLong("lastActiveTime", 0);
+        long timeout =60 * 1000; // 1 phút
+        long currentTime = System.currentTimeMillis();
+
+        if (isLoggedIn && (currentTime - lastActiveTime <= timeout)) {
+            // Cập nhật lại thời gian hoạt động
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putLong("lastActiveTime", currentTime);
+            editor.apply();
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+
         btnLogin.setOnClickListener(v -> {
             String username = edtLoginUN.getText().toString().trim();
             String password = edtLoginPassword.getText().toString().trim();
@@ -41,6 +60,14 @@ public class LoginActivity extends AppCompatActivity {
             Cursor cursor = db.query(DatabaseHelper.TABLE_USER, columns, selection, selectionArgs, null, null, null);
 
             if (cursor.moveToFirst()) {
+                // ==== Lưu session vào SharedPreferences ====
+//                SharedPreferences prefs = getSharedPreferences("UserSession", MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("isLoggedIn", true);
+                editor.putString("username", username);
+                editor.putLong("lastActiveTime", System.currentTimeMillis());
+                editor.apply();
+
                 Toast.makeText(this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
--- app/src/main/java/com/example/asm_ad/MainActivity.java
+++ app/src/main/java/com/example/asm_ad/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.asm_ad;
 
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 
@@ -20,28 +22,52 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefs = getSharedPreferences("UserSession", MODE_PRIVATE);
+        boolean isLoggedIn = prefs.getBoolean("isLoggedIn", false);
+        long lastActiveTime = prefs.getLong("lastActiveTime", 0);
+        long timeout = 60 * 1000; // 1 phút
 
-    protected void onCreate(Bundle savedInstanceState) {
+        long currentTime = System.currentTimeMillis();
+        if (!isLoggedIn || (currentTime - lastActiveTime > timeout)) {
+            // session hết hạn -> logout
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.clear();
+            editor.apply();
 
-        super.onCreate(savedInstanceState);
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        } else {
+            // Cập nhật thời gian hoạt động
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putLong("lastActiveTime", currentTime);
+            editor.apply();
+        }
+    }
 
-        EdgeToEdge.enable(this);
-
-        setContentView(R.layout.activity_main);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-
-            return insets;
-
-        });
-
+    @Override
+    public void onUserInteraction() {
+        super.onUserInteraction();
+        SharedPreferences prefs = getSharedPreferences("UserSession", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putLong("lastActiveTime", System.currentTimeMillis());
+        editor.apply();
     }
 
 }

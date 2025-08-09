8ed752d6e3d2f7d2c156ea751471eb4fb0a8d49f
mchinddd <chinh05102005@gmail.com>
2025-08-09 18:59:39 +0700

    chuyen code logic sang tieng anh

--- app/src/main/java/com/example/asm_ad/DatabaseHelper.java
+++ app/src/main/java/com/example/asm_ad/DatabaseHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "UserDatabase.db";
-    public static final int DATABASE_VERSION = 7;
+    public static final int DATABASE_VERSION = 8;
 
     // --- Bảng User ---
     public static final String TABLE_USER = "User";
